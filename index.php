06717c3ace8c750b25d3fbe342136d0449bacf36
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2021-10-03 13:15:56 +0630

    add new file

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News-MM</title>
+
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- style css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+
+    <h1 class="bg-primary text-center fs-6">Hello Welcome to Myanmar News Website</h1>
+    <i class="fas fa-facebook" class="text-danger"></i>
+
+
+
+
+
+
+
+
+    <!-- JQuery -->
+    <script src="assets/js/jquery-3.6.0.js"></script>
+    <!-- bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- script js -->
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
